8bd5231409a5e460c7f393c3449dcd0599eda51d
Alan Tapia <alantapia@Alans-MacBook-Air.local>
2023-07-13 22:43:48 -0300

    mend

--- scripts/pdf.js
+++ scripts/pdf.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector(".pdf")
     .addEventListener("click", () =>
-      window.open("../Eco-Reciclaje/assets/alicia.pdf", "_blank")
+      window.open("../assets/alicia.pdf", "_blank")
     );
 });
